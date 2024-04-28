d59a08dcfe9f1345b3fd3b251a5e777b790b5995
Philipp Nitsche <s0573651@htw-berlin.de>
2024-04-29 00:29:19 +0200

    data field modification tracking working and first tests written

--- src/nofeardb/orm.py
+++ src/nofeardb/orm.py
@@ -1,13 +1,20 @@
+"""
+Handles entity models
+"""
+
 from .enums import DocumentStatus
 
 
 class Document:
+    """
+    Base class for all documents that should be stored in the database
+    """
 
     __documentname__ = None
 
     def __init__(self):
         self.__status__ = DocumentStatus.NEW
-        self.__attr_snap__ = {}
+        self.__changed_fields__ = []
 
     def get_document_name(self):
         if self.__documentname__ is not None:
@@ -15,13 +22,41 @@ class Document:
 
         return self.__class__.__name__.lower()
 
-    def create_attribute_snapshot(self):
-        pass
-
 
 class Relationship:
-    pass
+    """
+    Descriptor for a relationship between documents
+    """
+
+    def __init__(self):
+        self._name = None
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self._name]
+
+    def __set__(self, instance, value):
+        instance.__dict__[self._name] = value
 
 
 class Field:
-    pass
+    """
+    Descriptor for a data field in a document
+    """
+
+    def __init__(self):
+        self._name = None
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self._name]
+
+    def __set__(self, instance: Document, value):
+        if self._name not in instance.__changed_fields__:
+            instance.__changed_fields__.append(self._name)
+
+        instance.__dict__[self._name] = value
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,2 +1,75 @@
-def test_false():
-    assert False
+# pylint: skip-file
+
+from src.nofeardb.enums import DocumentStatus
+from src.nofeardb.orm import Document, Field, Relationship
+
+
+class TestDoc(Document):
+    __documentname__ = "test_doc"
+
+    testfield_1 = Field()
+    testfield_2 = Field()
+    testRel_1 = Relationship()
+
+
+def test_get_document_name_set():
+
+    class NameTestDoc(Document):
+        __documentname__ = "hello_world"
+
+    doc = NameTestDoc()
+    assert doc.get_document_name() == "hello_world"
+
+
+def test_get_document_name_not_set():
+
+    class NameTestDoc(Document):
+        pass
+
+    doc = NameTestDoc()
+    assert doc.get_document_name() == "nametestdoc"
+
+
+def test_changed_data_fields_modified_model():
+    doc = TestDoc()
+    doc.__status__ = DocumentStatus.MOD
+
+    doc.testfield_1 = "hello"
+    doc.testfield_2 = 2
+    doc.__changed_fields__ = []
+
+    assert doc.__changed_fields__ == []
+    assert doc.testfield_1 == "hello"
+    assert doc.testfield_2 == 2
+
+    doc.testfield_1 = "world"
+
+    assert doc.__changed_fields__ == ["testfield_1"]
+    assert doc.testfield_1 == "world"
+    assert doc.testfield_2 == 2
+
+    doc.testfield_2 = 3
+
+    assert doc.__changed_fields__ == ["testfield_1", "testfield_2"]
+    assert doc.testfield_1 == "world"
+    assert doc.testfield_2 == 3
+
+
+# def test_changed_data_fields_new_model():
+#     assert False
+
+
+# def test_changed_data_fields_deleted_model():
+#     assert False
+
+
+# def test_changed_relationship_fields_modified_model():
+#     assert False
+
+
+# def test_changed_relationship_fields_new_model():
+#     assert False
+
+
+# def test_changed_relationship_fields_deleted_model():
+#     assert False
