5328d6106f5f83a0b6f1515abddc808eebe92a95
Philipp Nitsche <s0573651@htw-berlin.de>
2024-06-17 16:49:10 +0200

    fixed test and pylint issues under windows

--- src/nofeardb/engine.py
+++ src/nofeardb/engine.py
@@ -2,6 +2,8 @@
 Engine Classes
 """
 
+# pylint: disable=dangerous-default-value
+
 import os
 import json
 import uuid
@@ -20,7 +22,7 @@ class StorageEngine:
     """Storage Engine Class"""
 
     def __init__(self, root: str):
-        self._root = root
+        self._root = os.path.normpath(root)
         self._models = []
 
     def register_models(self, models: List[type]):
@@ -38,103 +40,112 @@ class StorageEngine:
 
     def create_json(self, doc: Document) -> dict:
         """creates the json that should be stored for a new object"""
-        
-        json = {}
-        
+
+        doc_json = {}
+
         for name, attr in vars(doc.__class__).items():
-            json["id"] = str(doc.__id__)
+            doc_json["id"] = str(doc.__id__)
             if isinstance(attr, Field):
                 if name != doc.__primary_key_attribute__:
                     field_type: OrmDataType = getattr(doc, name + "__datatype")
-                    json[name] = field_type.serialize(getattr(doc, name))
-                    
+                    doc_json[name] = field_type.serialize(getattr(doc, name))
+
             if isinstance(attr, ManyToMany) or isinstance(attr, OneToMany):
-                json[name] = [str(rel.__id__) for rel in getattr(doc, name)]
-            
+                doc_json[name] = [str(rel.__id__)
+                                  for rel in getattr(doc, name)]
+
             if isinstance(attr, ManyToOne):
                 if getattr(doc, name) is not None:
-                    json[name] = str(getattr(doc, name).__id__)
+                    doc_json[name] = str(getattr(doc, name).__id__)
                 else:
-                    json[name] = None
-        
-        return json
+                    doc_json[name] = None
 
-    def update_json(self, json: dict, doc: Document) -> dict:
+        return doc_json
+
+    def update_json(self, json_to_update: dict, doc: Document) -> dict:
         """updates the json by modified fields of an object"""
-        
+
         for name, attr in vars(doc.__class__).items():
             if name in doc.__changed_fields__:
                 if isinstance(attr, Field):
                     field_type: OrmDataType = getattr(doc, name + "__datatype")
-                    json[name] = field_type.serialize(getattr(doc, name))
-                    
+                    json_to_update[name] = field_type.serialize(
+                        getattr(doc, name))
+
             if name in doc.__added_relationships__:
                 if isinstance(attr, ManyToMany) or isinstance(attr, OneToMany):
                     if doc.__added_relationships__[name] is not None:
                         for rel in doc.__added_relationships__[name]:
-                            if name in json.keys():
-                                if str(rel.__id__) not in json[name]:
-                                    json[name].append(str(rel.__id__))
+                            if name in json_to_update.keys():
+                                if str(rel.__id__) not in json_to_update[name]:
+                                    json_to_update[name].append(
+                                        str(rel.__id__))
                             else:
-                                json[name] = [str(rel.__id__)]
-                
+                                json_to_update[name] = [str(rel.__id__)]
+
                 if isinstance(attr, ManyToOne):
                     if doc.__added_relationships__[name] is not None:
                         for rel in doc.__added_relationships__[name]:
-                            json[name] = str(rel.__id__)
-            
+                            json_to_update[name] = str(rel.__id__)
+
             if name in doc.__removed_relationships__:
                 if isinstance(attr, ManyToMany) or isinstance(attr, OneToMany):
                     if doc.__removed_relationships__[name] is not None:
                         for rel in doc.__removed_relationships__[name]:
-                            if name in json.keys():
-                                if str(rel.__id__) in json[name]:
-                                    json[name].remove(str(rel.__id__))
-                
+                            if name in json_to_update.keys():
+                                if str(rel.__id__) in json_to_update[name]:
+                                    json_to_update[name].remove(
+                                        str(rel.__id__))
+
                 if isinstance(attr, ManyToOne):
                     if doc.__removed_relationships__[name] is not None:
                         for rel in doc.__removed_relationships__[name]:
-                            if json[name] == str(rel.__id__):
-                                json[name] = None
-                
-        return json
-    
-    def resolve_dependencies(self, doc: Document, dependencies: List[Document] = []) -> List[Document]:
+                            if json_to_update[name] == str(rel.__id__):
+                                json_to_update[name] = None
+
+        return json_to_update
+
+    def resolve_dependencies(
+        self, doc: Document, dependencies: List[Document] = []
+    ) -> List[Document]:
         """creates a stack with depending documents"""
-        
+
         dependencies = []
-            
+
         children = [doc]
-        
+
         while len(children) > 0:
             child = children.pop()
             if child not in dependencies:
                 dependencies.append(child)
-                
-            for name, attr in vars(child.__class__).items():                  
+
+            for name, attr in vars(child.__class__).items():
                 if isinstance(attr, ManyToMany) or isinstance(attr, OneToMany):
                     for rel in getattr(child, name):
                         if rel not in children and rel not in dependencies:
                             children.append(rel)
-                
+
                 if isinstance(attr, ManyToOne):
                     if getattr(child, name) is not None:
-                        if getattr(child, name) not in children and getattr(child, name) not in dependencies:
+                        if (
+                            getattr(child, name) not in children
+                            and getattr(child, name) not in dependencies
+                        ):
                             children.append(getattr(child, name))
-            
+
         return dependencies
-    
+
     def get_doc_basepath(self, doc: Document):
         """get the base file path for the document type"""
         return os.path.join(self._root, doc.get_document_name())
-    
+
     def _get_document_with_id_existing(self, doc: Document):
         """Checks wether a document with the same ID already exists."""
         doc_base_path = self.get_doc_basepath(doc)
-        existing_ids = [doc_name.split("__")[0] for doc_name in os.listdir(doc_base_path)]
+        existing_ids = [doc_name.split("__")[0]
+                        for doc_name in os.listdir(doc_base_path)]
         return str(doc.__id__) in existing_ids
-            
-    
+
     def _check_all_documents_can_be_written(self, docs: List[Document]):
         """
         Checks wether all documents can be created or updated (no id collision etc.).
@@ -144,19 +155,20 @@ class StorageEngine:
             if doc.__status__ == DocumentStatus.NEW:
                 if self._get_document_with_id_existing(doc):
                     raise RuntimeError(
-                        "Document " 
-                        + str(doc) 
-                        + " is marked as new, but an document with the same ID is already existing.")
+                        "Document "
+                        + str(doc)
+                        + " is marked as new, but an document with the same ID is already existing."
+                    )
             if doc.__status__ == DocumentStatus.MOD:
                 if not self._get_document_with_id_existing(doc):
                     raise RuntimeError(
-                        "The document " 
-                        + str(doc) 
+                        "The document "
+                        + str(doc)
                         + " is marked as modified, but no document with the id "
                         + str(doc.__id__) + " exists.")
-            
+
         return True
-    
+
     def _lock_docs(self, docs: List[Document]) -> List['DocumentLock']:
         locks: List['DocumentLock'] = []
         try:
@@ -164,18 +176,18 @@ class StorageEngine:
                 lock = DocumentLock(self, doc, expiration=10)
                 lock.lock()
         except DocumentLockException as e:
-            self._unlock_docs(locks) 
+            self._unlock_docs(locks)
             raise DocumentLockException from e
-                
+
         return locks
-    
+
     def _unlock_docs(self, locks: List['DocumentLock']):
         for lock in locks:
             try:
                 lock.release()
             except DocumentLockException:
                 pass
-    
+
     def _get_existing_document_file_name(self, doc: Document):
         """get the filename of the document if it is alreday persisted to disk"""
         doc_base_path = self.get_doc_basepath(doc)
@@ -183,16 +195,16 @@ class StorageEngine:
             if "__" in file:
                 if file.split("__")[0] == str(doc.__id__) and os.path.splitext(file)[1] != '.tmp':
                     return os.path.join(doc_base_path, file)
-                
+
         return None
-    
+
     def _read_document_from_disk(self, doc_path):
         if doc_path is not None:
-            with open(doc_path) as f:
+            with open(doc_path, encoding="utf-8") as f:
                 return json.load(f)
-            
+
         return None
-    
+
     def write_json(self, doc: Document):
         """writes the document data to disk"""
         previous_file = self._get_existing_document_file_name(doc)
@@ -202,23 +214,25 @@ class StorageEngine:
             data_to_write = self.update_json(previous_data, doc)
         else:
             data_to_write = self.create_json(doc)
-            
-        doc_path = os.path.join(self.get_doc_basepath(doc), doc.__id__ + "__" + doc.get_hash() + ".json")
+
+        doc_path = os.path.join(self.get_doc_basepath(
+            doc), doc.__id__ + "__" + doc.get_hash() + ".json")
         doc_temp_path = doc_path + ".tmp"
-            
-        with open(doc_temp_path, 'w') as f:
+
+        with open(doc_temp_path, 'w', encoding="utf-8") as f:
             json.dump(data_to_write, f)
-            
+
         if previous_file is not None:
             os.remove(previous_file)
-            
+
         os.rename(doc_temp_path, doc_path)
 
     def create(self, doc: Document):
         """create the document"""
         if doc.__status__ is not DocumentStatus.NEW:
-            raise RuntimeError("The document is not new. Only new documents can be created")
-        
+            raise RuntimeError(
+                "The document is not new. Only new documents can be created")
+
         dependencies = self.resolve_dependencies(doc)
         if self._check_all_documents_can_be_written(dependencies):
             locks = self._lock_docs(dependencies)
@@ -228,17 +242,18 @@ class StorageEngine:
                     or dep.__status__ == DocumentStatus.MOD
                 ):
                     self.write_json(dep)
-                    
+
             self._unlock_docs(locks)
-         
+
     def update(self, doc: Document):
         """update the document"""
         if doc.__status__ is DocumentStatus.NEW:
-            raise RuntimeError("The document is not persisted. Please run \'create\' before.")
-        
+            raise RuntimeError(
+                "The document is not persisted. Please run \'create\' before.")
+
         if doc.__status__ is DocumentStatus.DEL:
             raise RuntimeError("Deleted documents cannot be updated")
-        
+
         dependencies = self.resolve_dependencies(doc)
         if self._check_all_documents_can_be_written(dependencies):
             locks = self._lock_docs(dependencies)
@@ -248,7 +263,7 @@ class StorageEngine:
                     or dep.__status__ == DocumentStatus.MOD
                 ):
                     self.write_json(dep)
-                    
+
             self._unlock_docs(locks)
 
     def delete(self, doc: Document):
@@ -256,24 +271,25 @@ class StorageEngine:
 
     def read(self, doc_type: Document, query_filter: QueryFilter = None):
         """read the document"""
-        
-        
+
+
 class DocumentLock:
-    
+    """A Lock for a specific document"""
+
     def __init__(self, storage_engine: StorageEngine, document: Document, expiration: int = 60):
         self._lock_id = uuid.uuid4()
         self.__document = document
         self.__engine = storage_engine
         self.__lock_path = os.path.join(
-            self.__engine.get_doc_basepath(self.__document), 
+            self.__engine.get_doc_basepath(self.__document),
             str(self.__document.__id__) + ".lock"
-            )
+        )
         self.__dateformat = '%Y-%m-%d %H:%M:%S'
         self.__expiration = expiration
-        
+
     def _is_lock_expired(self):
         try:
-            with open(self.__lock_path, "r") as lock_file:
+            with open(self.__lock_path, "r", encoding="utf-8") as lock_file:
                 lines = lock_file.readlines()
                 creation_date = datetime.strptime(lines[1], self.__dateformat)
                 return (datetime.now()-creation_date).total_seconds() > self.__expiration
@@ -281,45 +297,46 @@ class DocumentLock:
             return True
         except ValueError:
             return True
-    
+
     def _is_owner(self):
         try:
-            with open(self.__lock_path, "r") as lock_file:
+            with open(self.__lock_path, "r", encoding="utf-8") as lock_file:
                 lines = lock_file.readlines()
-                id = lines[0].strip()
-                return id == str(self._lock_id)
+                lock_id = lines[0].strip()
+                return lock_id == str(self._lock_id)
         except IndexError:
             return False
-        
+
     def _cleanup_old_lock(self):
         if os.path.exists(self.__lock_path):
             os.remove(self.__lock_path)
-        
+
     def lock(self):
         """locks a document"""
-        if self.isLocked():
+        if self.is_locked():
             raise DocumentLockException("Document is already locked.")
-        
+
         self._cleanup_old_lock()
-            
-        with open(self.__lock_path, "a") as lock_file:
+
+        with open(self.__lock_path, "a", encoding="utf-8") as lock_file:
             lock_file.writelines(
                 [self._lock_id,
-                datetime.now().strftime(self.__dateformat)]
+                 datetime.now().strftime(self.__dateformat)]
             )
-        
+
     def release(self):
         """releases a document lock"""
         if os.path.exists(self.__lock_path):
             if self._is_owner() or self._is_lock_expired():
                 os.remove(self.__lock_path)
             else:
-                raise DocumentLockException("Cannot release a lock that is hold by someone else.")
-        
-    def isLocked(self) -> bool:
+                raise DocumentLockException(
+                    "Cannot release a lock that is hold by someone else.")
+
+    def is_locked(self) -> bool:
         """returns wether a document is currently locked"""
         if os.path.exists(self.__lock_path):
             if not self._is_lock_expired():
                 return True
-            
+
         return False
