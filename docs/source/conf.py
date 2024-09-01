ff83c91cdd00d217721aba71f06821256b5efd91
Philipp Nitsche <s0573651@htw-berlin.de>
2024-09-01 23:44:46 +0200

    fixed docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,5 +34,5 @@ autosummary_generate = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'alabaster'
 html_static_path = ['_static']
